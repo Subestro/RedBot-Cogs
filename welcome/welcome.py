83c464c26ffce2a96c672f157170bed170699b88
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-12 01:55:07 -0800

    Update welcome.py

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -126,7 +126,7 @@ class welcome(commands.Cog):
         font = ImageFont.truetype(BytesIO(requests.get(font_url).content), size=42)
         member_count = len(member.guild.members)
         member_count_text = f"You are the {inflector.ordinal(member_count)} user"
-        member_count_text = fill(member_count_text, initial_indent="", subsequent_indent="", width=len(member_count_text), placeholder="_")
+        member_count_text = fill(member_count_text, initial_indent="", subsequent_indent="", width=len(member_count_text), placeholder="______")
         text_width, text_height = draw.textsize(member_count_text, font=font)
         #text_position = ((welcome_image_width - text_width) // 2, text_position[1] + text_height)
         text_position = ((welcome_image_width - text_width) // 2, welcome_image_height - text_height - 70)
