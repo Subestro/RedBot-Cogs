1ffa66c5bbc6a3f6e88c6e1bbb7c7c27c8368aef
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-16 18:20:02 -0800

    Update FreeGamesChecker.py

--- FreeGamesChecker/FreeGamesChecker.py
+++ FreeGamesChecker/FreeGamesChecker.py
@@ -21,9 +21,7 @@ class FreeGamesChecker(commands.Cog):
     @commands.command(name='tfg', description='Test whether free games notifications are enabled and working.')
     async def testfreegames(self, ctx):
         if self.channel:
-            # Select a random free game from the list of free games
-            game = random.choice(self.free_games)
-            await ctx.send(f"Random free game: {game}")
+            await ctx.send("Free games notifications are enabled and working!")
         else:
             await ctx.send("Free games notifications are disabled or not set up.")
 
