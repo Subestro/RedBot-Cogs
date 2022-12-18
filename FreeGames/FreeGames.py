e13610d86eb13166482fecb4330523b023e444a1
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-17 21:21:04 -0800

    Update FreeGames.py

--- FreeGames/FreeGames.py
+++ FreeGames/FreeGames.py
@@ -60,7 +60,7 @@ class FreeGames(commands.Cog):
                  #embed = discord.Embed(title=game.name, color=0x00FF00)
                  embed = discord.Embed(color=0x00FF00)
                  embed.add_field(name=game.name, value=game.url, inline=False)
-                 embed.title = "{game.name}"
+                 embed.title = "Free game from the Epic Games store"
             await ctx.send(embed=embed)
         else:
             await ctx.send("No free games could be found.")
