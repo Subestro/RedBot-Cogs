7d04998a47a67cc8cfe7082a2b6f205870ecde42
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-17 21:48:26 -0800

    Update FreeGames.py

--- FreeGames/FreeGames.py
+++ FreeGames/FreeGames.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import requests
+import datetime
 from requests.exceptions import HTTPError, Timeout
 from redbot.core import commands, checks
 
@@ -57,7 +58,32 @@ class FreeGames(commands.Cog):
         # Send the list of free games in an embed
         if free_games:
             for game in free_games:
-                 embed = discord.Embed(title=game.name, color=0x00FF00)
-            await ctx.send(embed=embed)
+                # Create embed message
+                embed = discord.Embed(title=game.name, color=0x00FF00)
+
+                # Add original price field
+                original_price = i["price"]["totalPrice"]["originalPrice"]["fmtPrice"]["display"]
+                embed.add_field(name="Original price", value=f"~~{original_price}~~", inline=False)
+
+                # Add "Free" field
+                embed.add_field(name="Status", value="Free", inline=False)
+
+                # Calculate remaining time
+                end_time = datetime.datetime.fromtimestamp(i["promotions"]["promotionalOffers"][0]["endsAt"])
+                remaining_time = end_time - datetime.datetime.now()
+
+                # Format remaining time
+                if remaining_time.days > 0:
+                    remaining_time_formatted = f"{remaining_time.days} days, {remaining_time.hours} hours, {remaining_time.minutes} minutes"
+                elif remaining_time.hours > 0:
+                    remaining_time_formatted = f"{remaining_time.hours} hours, {remaining_time.minutes} minutes"
+                else:
+                    remaining_time_formatted = f"{remaining_time.minutes} minutes"
+
+                # Add remaining time field
+                embed.add_field(name="Remaining time", value=remaining_time_formatted, inline=False)
+
+                # Send embed message
+                await ctx.send(embed=embed)
         else:
             await ctx.send("No free games could be found.")
