03414668db92033d870358c1777c1449329f819a
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-18 02:00:46 -0800

    Update FreeGames.py

--- FreeGames/FreeGames.py
+++ FreeGames/FreeGames.py
@@ -63,6 +63,7 @@ class FreeGames(commands.Cog):
                 embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Epic_games_store_logo.svg/800px-Epic_games_store_logo.svg.png")
                 embed.description = f"~~${game.original_price}~~ | **Free**"
                 embed.set_image(url=game.poster_url)
+                embed.add_field(name="\u200b", value="[Button Text](https://example.com)", inline=True)
                 await ctx.send(embed=embed)
         else:
             await ctx.send("No free games could be found.")
