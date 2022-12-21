b6d64cda5f0d8d49d817d098301c822d0fb7a9d2
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-21 03:02:06 -0800

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -5,40 +5,46 @@ import trakt
 import urllib.parse
 from redbot.core import commands
 
-class rTrakt(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+import requests
 
-    @commands.command()
-    async def watching(self, ctx, access_token=None):
-        # Check if the OAuth access token is present
-        if access_token is None:
-            # Build the authorize URL with the necessary parameters
-            params = {
-                "client_id": "your_client_id_here",
-                "redirect_uri": "your_redirect_uri_here",
-                "response_type": "code",
-                "state": "random_string_here"
-            }
-            authorize_url = "https://trakt.tv/oauth/authorize?" + urllib.parse.urlencode(params)
+@commands.command()
+async def watching(self, ctx, access_token=None):
+    # Check if the OAuth access token is present
+    if access_token is None:
+        # Send a message to the user with instructions on how to obtain the access token
+        embed = discord.Embed(
+            title="Access Token Required",
+            description="To use this command, you must provide a valid OAuth access token. You can obtain a token by visiting the following URL and following the instructions: https://trakt.tv/oauth/applications",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
+        return
 
-            # Send a message to the user with a link to the authorize URL
-            embed = discord.Embed(
-                title="Authorization Required",
-                description=f"To use this command, you must authorize this app to access your Trakt account. Click [here]({authorize_url}) to begin the authorization process.",
-                color=discord.Color.red()
-            )
-            await ctx.send(embed=embed)
-            return
+    # Make a GET request to the /sync/playback endpoint
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+    response = requests.get("https://api.trakt.tv/sync/playback", headers=headers)
 
-        # Create a new Client object and authenticate using the OAuth access token
-        client = pytrakt.Client(access_token=access_token)
+    # Check the response status code
+    if response.status_code == 200:
+        # Parse the response data
+        data = response.json()
 
         # Get the user's current activity
-        activity = client.activity.watching()
+        activity = data["item"]["title"]
 
         # Create a new Game object with the user's current activity as the name
         game = discord.Game(name=activity)
 
         # Update the bot's rich presence with the new Game object
         await self.bot.change_presence(activity=game)
+    else:
+        # If the request fails, send a message to the user
+        embed = discord.Embed(
+            title="Error",
+            description=f"An error occurred while trying to get your current playback activity. The status code was {response.status_code}.",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=embed)
