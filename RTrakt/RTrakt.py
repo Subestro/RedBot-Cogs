deca19db5777e955b21b52d4a6392bef06cb2c5b
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-20 18:38:49 -0800

    Update RTrakt.py

--- RTrakt/RTrakt.py
+++ RTrakt/RTrakt.py
@@ -10,6 +10,7 @@ class RTrakt(commands.Cog):
         self.trakt_client_id = None
         self.trakt_client_secret = None
         self.imdb_api_key = None
+        self.trakt_redirect_url = None
 
         # Read the configuration values from a file
         config = configparser.ConfigParser()
@@ -17,11 +18,12 @@ class RTrakt(commands.Cog):
         if 'trakt' in config:
             self.trakt_client_id = config['trakt'].get('client_id', None)
             self.trakt_client_secret = config['trakt'].get('client_secret', None)
+            self.trakt_redirect_url = config['trakt'].get('redirect_url', None)
         if 'imdb' in config:
             self.imdb_api_key = config['imdb'].get('api_key', None)
 
     @commands.command(help="Set the Trakt API client ID")
-    async def settrakt(self, ctx, client_id: str):
+    async def Rsettrakt(self, ctx, client_id: str):
         self.trakt_client_id = client_id
         # Save the client ID to the configuration file
         config = configparser.ConfigParser()
@@ -33,7 +35,7 @@ class RTrakt(commands.Cog):
             config.write(configfile)
 
     @commands.command(help="Set the Trakt API client secret")
-    async def setsecret(self, ctx, client_secret: str):
+    async def Rsetsecret(self, ctx, client_secret: str):
         self.trakt_client_secret = client_secret
         # Save the client secret to the configuration file
         config = configparser.ConfigParser()
@@ -43,9 +45,21 @@ class RTrakt(commands.Cog):
         config['trakt']['client_secret'] = client_secret
         with open('rTrakt_config.ini', 'w') as configfile:
             config.write(configfile)
+    @commands.command(help="Set the Trakt API redirect URL")
+    async def Rsetredirect(self, ctx, redirect_url: str):
+        self.trakt_redirect_url = redirect_url
+        # Save the redirect URL to the configuration file
+        config = configparser.ConfigParser()
+        config.read('rTrakt_config.ini')
+        if 'trakt' not in config:
+            config['trakt'] = {}
+        config['trakt']['redirect_url'] = redirect_url
+        with open('rTrakt_config.ini', 'w') as configfile:
+            config.write(configfile)
+
 
     @commands.command(help="Set the IMDb API key")
-    async def setimdb(self, ctx, api_key: str):
+    async def Rsetimdb(self, ctx, api_key: str):
         self.imdb_api_key = api_key
         # Save the API key to the configuration file
         config = configparser.ConfigParser()
