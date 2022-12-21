387d4969a14267fb7042b2d272f8d711f65866ed
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-21 02:43:45 -0800

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,37 +1,44 @@
 import discord
+import pytrakt
+import requests
+import urllib.parse
+
 from redbot.core import commands
-import trakt
-import asyncio
 
 class rTrakt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.trakt_api = trakt.TraktAPI(client_id='YOUR_CLIENT_ID', client_secret='YOUR_CLIENT_SECRET')
 
     @commands.command()
-    async def set_watching(self, ctx):
-        # Redirect the user to the Trakt authorization URL
-        auth_url = self.trakt_api.auth.authorization_url('urn:ietf:wg:oauth:2.0:oob')
-        await ctx.send(f'Please visit the following URL to authorize the bot to access your Trakt account: {auth_url}')
+    async def watching(self, ctx, access_token=None):
+        # Check if the OAuth access token is present
+        if access_token is None:
+            # Build the authorize URL with the necessary parameters
+            params = {
+                "client_id": "your_client_id_here",
+                "redirect_uri": "your_redirect_uri_here",
+                "response_type": "code",
+                "state": "random_string_here"
+            }
+            authorize_url = "https://trakt.tv/oauth/authorize?" + urllib.parse.urlencode(params)
 
-        # Wait for the user to enter the authorization code
-        def check(m):
-            return m.channel == ctx.channel and m.author == ctx.author
-        try:
-            msg = await self.bot.wait_for('message', check=check, timeout=60.0)
-        except asyncio.TimeoutError:
-            return await ctx.send('Timed out waiting for authorization code.')
+            # Send a message to the user with a link to the authorize URL
+            embed = discord.Embed(
+                title="Authorization Required",
+                description=f"To use this command, you must authorize this app to access your Trakt account. Click [here]({authorize_url}) to begin the authorization process.",
+                color=discord.Color.red()
+            )
+            await ctx.send(embed=embed)
+            return
 
-        # Exchange the authorization code for an access token
-        code = msg.content
-        access_token = self.trakt_api.auth.exchange_code_for_token(code)
-        self.trakt_api.set_access_token(access_token)
+        # Create a new Client object and authenticate using the OAuth access token
+        client = pytrakt.Client(access_token=access_token)
 
-        # Set the bot's rich presence to show what the user is currently watching
-        currently_watching = self.trakt_api.sync.get_playback()
-        activity = discord.Game(name=currently_watching.current.title)
-        await self.bot.change_presence(activity=activity)
-        await ctx.send('Successfully set rich presence to show what you are currently watching.')
+        # Get the user's current activity
+        activity = client.activity.watching()
 
-def setup(bot):
-    bot.add_cog(rTrakt(bot))
+        # Create a new Game object with the user's current activity as the name
+        game = discord.Game(name=activity)
+
+        # Update the bot's rich presence with the new Game object
+        await self.bot.change_presence(activity=game)
