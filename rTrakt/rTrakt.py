709456aee5e72152875f369f84d9b95a4c62f926
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-21 00:32:14 -0800

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,88 +1,33 @@
 import discord
-import requests
 from redbot.core import commands
 import trakt
-import configparser
 
 class rTrakt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.trakt_client_id = None
-        self.trakt_client_secret = None
-        self.omdb_api_key = None
-        self.trakt_redirect_url = None
 
-        # Read the configuration values from a file
-        config = configparser.ConfigParser()
-        config.read('rTrakt_config.ini')
-        if 'trakt' in config:
-            self.trakt_client_id = config['trakt'].get('client_id', None)
-            self.trakt_client_secret = config['trakt'].get('client_secret', None)
-            self.trakt_redirect_url = config['trakt'].get('redirect_url', None)
-        if 'omdb' in config:
-            self.omdb_api_key = config['omdb'].get('api_key', None)
+    @commands.command()
+    async def set_trakt_keys(self, ctx, client_id: str, client_secret: str, access_token: str):
+        """Sets the Trakt API keys and access token."""
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = access_token
+        await ctx.send("Trakt API keys and access token set successfully!")
 
-    @commands.command(help="Set the Trakt API client ID")
-    async def Rsettrakt(self, ctx, client_id: str):
-        self.trakt_client_id = client_id
-        # Save the client ID to the configuration file
-        config = configparser.ConfigParser()
-        config.read('rTrakt_config.ini')
-        if 'trakt' not in config:
-            config['trakt'] = {}
-        config['trakt']['client_id'] = client_id
-        with open('rTrakt_config.ini', 'w') as configfile:
-            config.write(configfile)
+    @commands.command()
+    async def update_presence(self, ctx):
+        # Authenticate with the Trakt Scrobbler API
+        trakt.init(self.client_id, self.client_secret)
+        access_token = trakt.auth.oauth(self.access_token)
 
-    @commands.command(help="Set the Trakt API client secret")
-    async def Rsetsecret(self, ctx, client_secret: str):
-        self.trakt_client_secret = client_secret
-        # Save the client secret to the configuration file
-        config = configparser.ConfigParser()
-        config.read('rTrakt_config.ini')
-        if 'trakt' not in config:
-            config['trakt'] = {}
-        config['trakt']['client_secret'] = client_secret
-        with open('rTrakt_config.ini', 'w') as configfile:
-            config.write(configfile)
-    @commands.command(help="Set the Trakt API redirect URL")
-    async def Rsetredirect(self, ctx, redirect_url: str):
-        self.trakt_redirect_url = redirect_url
-        # Save the redirect URL to the configuration file
-        config = configparser.ConfigParser()
-        config.read('rTrakt_config.ini')
-        if 'trakt' not in config:
-            config['trakt'] = {}
-        config['trakt']['redirect_url'] = redirect_url
-        with open('rTrakt_config.ini', 'w') as configfile:
-            config.write(configfile)
+        # Retrieve the details of the currently-playing media
+        scrobble_state = trakt.sync.scrobble.state(access_token)
 
-    @commands.command(help="Set the OMDb API key")
-    async def Rsetomdb(self, ctx, api_key: str):
-        self.omdb_api_key = api_key
-        # Save the API key to the configuration file
-        config = configparser.ConfigParser()
-        config.read('rTrakt_config.ini')
-        if 'omdb' not in config:
-            config['omdb'] = {}
-        config['omdb']['api_key'] = api_key
-        with open('rTrakt_config.ini', 'w') as configfile:
-            config.write(configfile)
+        # Create a rich presence object based on the media type
+        if scrobble_state.type == "episode":
+            game = discord.Game(name=f"{scrobble_state.show.title} - S{scrobble_state.episode.season}E{scrobble_state.episode.number} - {scrobble_state.episode.title}", type=discord.ActivityType.watching)
+        elif scrobble_state.type == "movie":
+            game = discord.Game(name=f"{scrobble_state.movie.title}", type=discord.ActivityType.watching)
 
-        # Get the current user's Trakt account information
-        user = trakt.users.me()
-
-        # Get the currently playing media on the user's Trakt account
-        playing = user.watching()
-
-        # Search for the show or movie on OMDb
-        query = playing.title
-        endpoint = f"http://www.omdbapi.com/?apikey={self.omdb_api_key}&s={query}"
-        response = requests.get(endpoint)
-        data = response.json()
-
-        # Get the poster image URL from the OMDb response
-        poster_url = data["Search"][0]["Poster"]
-
-        # Send the poster image URL as a message in Discord
-        await ctx.send(poster_url)
+        # Update the bot's rich presence
+        await self.bot.change_presence(activity=game)
