bf01595061129020ec9694441eb6dbb1c61ef84d
PythonMC <120358755+Subestro@users.noreply.github.com>
2022-12-22 02:15:25 -0800

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,48 +1,74 @@
-import asyncio
-import requests
 import discord
+import aiohttp
+import asyncio
+import os
+
 from discord.ext import commands
-from redbot.core import Config
+from redbot.core import Config, checks, commands
 
 class rTrakt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=3393734173)
-        default_global = {"api_key": None}
-        self.config.register_global(**default_global)
-        self.trakt_api_key = self.config.api_key()
-        self.update_presence()
-        self.loop_task = self.bot.loop.create_task(self.loop)
-
-    async def update_presence(self):
-        # Make a request to the Trakt API to retrieve the current scrobbler status
-        headers = {
-            "Content-Type": "application/json",
-            "trakt-api-key": self.trakt_api_key,
+        self.config = Config.get_conf(self, identifier=1234567890)
+        default_global = {
+            "api_key": None,
         }
-        response = requests.get("https://api.trakt.tv/users/me/watching", headers=headers)
-        data = response.json()
-
-        # If we're currently watching something, create an activity object and update the rich presence
-        if data["watching"]:
-            show = data["watching"]["show"]
-            episode = data["watching"]["episode"]
-            activity = discord.Activity(
-                name=f"{show['title']} - {episode['title']}",
-                type=discord.ActivityType.watching,
-            )
-            await self.bot.change_presence(activity=activity)
-
-    async def loop(self):
-        while True:
-            self.update_presence()
-            await asyncio.sleep(30)
+        self.config.register_global(**default_global)
 
     @commands.command()
+    @checks.is_owner()
     async def setapikey(self, ctx, api_key: str):
+        """Set the API key for the Trakt API"""
         await self.config.api_key.set(api_key)
-        self.trakt_api_key = api_key
-        await ctx.send("Successfully set Trakt API key!")
+        await ctx.send("API key set successfully.")
+
+    async def update_rich_presence(self):
+        """Update the bot's rich presence with the scrobbler status"""
+        api_key = await self.config.api_key()
+        if api_key is None:
+            return
+
+        # Use the Trakt API to retrieve the scrobbler status
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.trakt.tv/sync/playback", headers={
+                "Content-Type": "application/json",
+                "trakt-api-key": api_key,
+                "trakt-api-version": "2",
+            }) as resp:
+                if resp.status != 200:
+                    return
+                data = await resp.json()
+
+        # Update the bot's rich presence with the scrobbler status
+        if data["type"] == "episode":
+            activity = discord.Activity(
+                name=data["episode"]["title"],
+                type=discord.ActivityType.watching,
+                details=data["show"]["title"],
+                state=f"Season {data['episode']['season']} Episode {data['episode']['number']}",
+            )
+        elif data["type"] == "movie":
+            activity = discord.Activity(
+                name=data["movie"]["title"],
+                type=discord.ActivityType.watching,
+                details=data["movie"]["title"],
+            )
+        await self.bot.change_presence(activity=activity)
+
+    async def on_ready(self):
+        """Start updating the rich presence with the scrobbler status"""
+        api_key = await self.config.api_key()
+        if api_key is None:
+            return
+
+        # Start updating the rich presence in a loop
+        self.task = self.bot.loop.create_task(self.update_rich_presence_loop())
+
+    async def update_rich_presence_loop(self):
+        """Update the rich presence in a loop"""
+        while True:
+            await self.update_rich_presence()
+            await asyncio.sleep(60)
 
 def setup(bot):
     bot.add_cog(rTrakt(bot))
