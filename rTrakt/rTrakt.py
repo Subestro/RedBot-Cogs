f63617ef1c534c897e9a0a169d0b180d869b0596
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-13 18:15:26 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,22 +1,30 @@
-import discord
-from discord.ext import commands
+from redbot.core import commands, Config
 from trakt import Trakt
 
 class TraktCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.client_id = 'YOUR_TRAKT_CLIENT_ID'
-        self.client_secret = 'YOUR_TRAKT_CLIENT_SECRET'
+        self.config = Config.get_conf(self, identifier=1234567890)  # Replace with a unique identifier of your choice
 
-    def authenticate(self):
+        default_global = {
+            "trakt_client_id": "",
+            "trakt_client_secret": ""
+        }
+
+        self.config.register_global(**default_global)
+
+    async def initialize(self):
+        trakt_client_id = await self.config.trakt_client_id()
+        trakt_client_secret = await self.config.trakt_client_secret()
+
         Trakt.configuration.defaults.client(
-            id=self.client_id,
-            secret=self.client_secret
+            id=trakt_client_id,
+            secret=trakt_client_secret
         )
 
     @commands.command()
     async def search_movie(self, ctx, movie_title):
-        self.authenticate()
+        await self.initialize()
         results = Trakt['search'].movie(query=movie_title)
         if results:
             movie = results[0]
@@ -26,7 +34,7 @@ class TraktCog(commands.Cog):
 
     @commands.command()
     async def search_show(self, ctx, show_title):
-        self.authenticate()
+        await self.initialize()
         results = Trakt['search'].show(query=show_title)
         if results:
             show = results[0]
