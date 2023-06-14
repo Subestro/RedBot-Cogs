bbda7b3f81a6946ab8cf4e1da0c5ca87186eb7d2
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-13 19:22:36 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -36,26 +36,6 @@ class rTrakt(commands.Cog):
                 if activity.name and activity.type == discord.ActivityType.streaming:
                     await self.config.target_channel.send(f"Currently watching: {activity.name}")
 
-    @commands.command()
-    async def traktid(self, ctx, client_id):
-        await self.config.trakt_client_id.set(client_id)
-        await ctx.send("Trakt client ID set.")
-
-    @commands.command()
-    async def traktst(self, ctx, client_secret):
-        await self.config.trakt_client_secret.set(client_secret)
-        await ctx.send("Trakt client secret set.")
-
-    @commands.command()
-    async def trakttoken(self, ctx, access_token):
-        await self.config.trakt_access_token.set(access_token)
-        await ctx.send("Trakt access token set.")
-
-    @commands.command()
-    async def set_channel(self, ctx, channel: discord.TextChannel):
-        await self.config.target_channel.set(channel.id)
-        await ctx.send(f"Target channel set to {channel.mention}.")
-
 
 def setup(bot):
     cog = rTrakt(bot)
