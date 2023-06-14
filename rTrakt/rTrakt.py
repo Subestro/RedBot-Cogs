bdc2911931aaf625168ea73808b3f3abbce32f0b
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-13 17:35:04 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,4 +1,5 @@
-from redbot.core import commands, Config
+from discord.ext import commands
+from redbot.core import Config
 from urllib.parse import urlencode
 
 class rTrakt(commands.Cog):
@@ -12,12 +13,12 @@ class rTrakt(commands.Cog):
             "trakt_access_token": ""
         }
         self.config.register_guild(**default_guild)
-    
+
     @commands.command()
     async def traktlogin(self, ctx):
         client_id = await self.config.guild(ctx.guild).trakt_client_id()
         redirect_uri = await self.config.guild(ctx.guild).trakt_redirect_uri()
-        
+
         oauth_params = {
             "response_type": "code",
             "client_id": client_id,
@@ -25,9 +26,9 @@ class rTrakt(commands.Cog):
             "state": "random_state_string",
         }
         oauth_url = f"https://trakt.tv/oauth/authorize?{urlencode(oauth_params)}"
-        
+
         await ctx.send(f"Click the following link to authorize Trakt:\n{oauth_url}")
-    
+
     @commands.command()
     async def settrakttoken(self, ctx, access_token):
         await self.config.guild(ctx.guild).trakt_access_token.set(access_token)
