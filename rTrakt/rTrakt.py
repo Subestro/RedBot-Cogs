def92c5eaa8dee6eb97d2358e572325c5083846e
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-13 19:29:51 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,66 +1,41 @@
 import discord
-from redbot.core import commands, Config
+from discord.ext import commands
 from trakt import Trakt
-
+from redbot.core import Config
 
 class rTrakt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=2224567891)  # Replace with a unique identifier of your choice
-
+        self.config = Config.get_conf(self, identifier=1234567890)
         default_global = {
-            "trakt_client_id": "",
-            "trakt_client_secret": "",
-            "trakt_access_token": "",
-            "target_channel": None
+            "client_id": None,
+            "client_secret": None
         }
-
         self.config.register_global(**default_global)
 
-    @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        if before.id != self.bot.user.id:
+    @commands.command()
+    async def trakt(self, ctx):
+        client_id = await self.config.client_id()
+        client_secret = await self.config.client_secret()
+        if not client_id or not client_secret:
+            await ctx.send("Please set the Trakt API client ID and secret using the `settrakt` command.")
             return
-
-        if before.activity != after.activity:
-            await self.send_currently_watching(after.activity)
-
-    async def send_currently_watching(self, activity):
-        if isinstance(activity, discord.Streaming):
-            trakt_access_token = await self.config.trakt_access_token()
-            if trakt_access_token:
-                Trakt.configuration.defaults.oauth.from_response(
-                    response={"access_token": trakt_access_token}
-                )
-
-                if activity.name and activity.type == discord.ActivityType.streaming:
-                    target_channel_id = await self.config.target_channel()
-                    if target_channel_id:
-                        target_channel = self.bot.get_channel(target_channel_id)
-                        if target_channel:
-                            await target_channel.send(f"Currently watching: {activity.name}")
+        Trakt.configuration.defaults.client(
+            id=client_id,
+            secret=client_secret,
+            redirect_uri='urn:ietf:wg:oauth:2.0:oob'
+        )
+        Trakt.configuration.defaults.oauth.from_response(
+            input('Enter the pin given at %s : ' % Trakt['oauth'].authorize_url())
+        )
+        movie = Trakt['search'].movie('The Matrix')[0]
+        await ctx.send(f"Watching {movie.title} on {movie.year}.")
 
     @commands.command()
-    async def traktid(self, ctx, client_id):
-        await self.config.trakt_client_id.set(client_id)
-        await ctx.send("Trakt client ID set.")
-
-    @commands.command()
-    async def traktst(self, ctx, client_secret):
-        await self.config.trakt_client_secret.set(client_secret)
-        await ctx.send("Trakt client secret set.")
-
-    @commands.command()
-    async def trakttoken(self, ctx, access_token):
-        await self.config.trakt_access_token.set(access_token)
-        await ctx.send("Trakt access token set.")
-
-    @commands.command()
-    async def set_channel(self, ctx, channel: discord.TextChannel):
-        await self.config.target_channel.set(channel.id)
-        await ctx.send(f"Target channel set to {channel.mention}.")
-
+    async def settrakt(self, ctx, client_id: str, client_secret: str):
+        await self.config.client_id.set(client_id)
+        await self.config.client_secret.set(client_secret)
+        await ctx.send("Trakt API client ID and secret set.")
 
 def setup(bot):
-    cog = rTrakt(bot)
-    bot.add_cog(cog)
+    bot.add_cog(rTrakt(bot))
