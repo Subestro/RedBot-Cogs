74ade0b628528d8d1529d825b46c710338ecd11e
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-13 19:08:24 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,5 +1,6 @@
+import discord
+import requests
 from redbot.core import commands, Config
-from trakt import Trakt
 
 class rTrakt(commands.Cog):
     def __init__(self, bot):
@@ -8,19 +9,20 @@ class rTrakt(commands.Cog):
 
         default_global = {
             "trakt_client_id": "",
-            "trakt_client_secret": ""
+            "trakt_client_secret": "",
+            "trakt_access_token": "",
+            "target_channel_id": 0  # Replace with the actual channel ID where you want the message to be sent
         }
 
         self.config.register_global(**default_global)
 
-    async def initialize(self):
-        trakt_client_id = await self.config.trakt_client_id()
-        trakt_client_secret = await self.config.trakt_client_secret()
-
-        Trakt.configuration.defaults.client(
-            id=trakt_client_id,
-            secret=trakt_client_secret
-        )
+    async def send_currently_watching(self, item):
+        channel_id = await self.config.target_channel_id()
+        channel = self.bot.get_channel(channel_id)
+        if channel:
+            await channel.send(f"Currently watching: {item['title']}")
+        else:
+            print(f"Channel with ID {channel_id} not found.")
 
     @commands.command()
     async def id(self, ctx, client_id):
@@ -33,24 +35,33 @@ class rTrakt(commands.Cog):
         await ctx.send("Trakt client secret set successfully.")
 
     @commands.command()
-    async def search_movie(self, ctx, movie_title):
-        await self.initialize()
-        results = Trakt['search'].query(query=movie_title, limit=1, media_type='movie')
-        if results:
-            movie = results[0]['movie']
-            await ctx.send(f"Title: {movie['title']}\nYear: {movie['year']}")
-        else:
-            await ctx.send('No movie found.')
+    async def trakttoken(self, ctx, access_token):
+        await self.config.trakt_access_token.set(access_token)
+        await ctx.send("Trakt access token set successfully.")
 
     @commands.command()
-    async def search_show(self, ctx, show_title):
-        await self.initialize()
-        results = Trakt['search'].query(query=show_title, limit=1, media_type='show')
-        if results:
-            show = results[0]['show']
-            await ctx.send(f"Title: {show['title']}\nYear: {show['year']}")
-        else:
-            await ctx.send('No show found.')
+    async def set_channel(self, ctx, channel: discord.TextChannel):
+        await self.config.target_channel_id.set(channel.id)
+        await ctx.send(f"Target channel set to {channel.mention}.")
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        # Check if the user's activity has changed
+        if before.activity != after.activity and isinstance(after.activity, discord.Streaming):
+            access_token = await self.config.trakt_access_token()
+            if access_token:
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {access_token}"
+                }
+                response = requests.get("https://api.trakt.tv/users/me/watching", headers=headers)
+                if response.status_code == 200:
+                    watching_item = response.json()
+                    await self.send_currently_watching(watching_item)
+                else:
+                    print(f"Trakt API request failed with status code {response.status_code}.")
+            else:
+                print("Trakt access token not set.")
 
 def setup(bot):
     bot.add_cog(rTrakt(bot))
