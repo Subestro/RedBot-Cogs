63e0ef603aec1393b9229deed3ad3159d10e3ee2
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-13 18:56:18 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,6 +1,7 @@
 from redbot.core import commands, Config
 from trakt import Trakt
 
+
 class rTrakt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -22,42 +23,36 @@ class rTrakt(commands.Cog):
             secret=trakt_client_secret
         )
 
-    @commands.group()
-    async def trakt(self, ctx):
-        """Trakt API settings"""
-        pass
-
-    @trakt.command()
+    @commands.command()
     async def id(self, ctx, client_id):
-        """Set the Trakt client ID"""
         await self.config.trakt_client_id.set(client_id)
-        await ctx.send("Trakt client ID has been set.")
+        await ctx.send("Trakt client ID set.")
 
-    @trakt.command()
+    @commands.command()
     async def st(self, ctx, client_secret):
-        """Set the Trakt client secret"""
         await self.config.trakt_client_secret.set(client_secret)
-        await ctx.send("Trakt client secret has been set.")
+        await ctx.send("Trakt client secret set.")
 
     @commands.command()
     async def search_movie(self, ctx, *, movie_title):
         await self.initialize()
-        results = Trakt['search'].query('movies', movie_title)
-        if results:
-            movie = results[0]
-            await ctx.send(f"Title: {movie.title}\nYear: {movie.year}")
+        results = Trakt['search'].query(query=movie_title, media_type='movie')
+        if results and results.get('movie'):
+            movie = results['movie'][0]
+            await ctx.send(f"Title: {movie['title']}\nYear: {movie['year']}")
         else:
             await ctx.send('No movie found.')
 
     @commands.command()
     async def search_show(self, ctx, *, show_title):
         await self.initialize()
-        results = Trakt['search'].query('shows', show_title)
-        if results:
-            show = results[0]
-            await ctx.send(f"Title: {show.title}\nYear: {show.year}")
+        results = Trakt['search'].query(query=show_title, media_type='show')
+        if results and results.get('show'):
+            show = results['show'][0]
+            await ctx.send(f"Title: {show['title']}\nYear: {show['year']}")
         else:
             await ctx.send('No show found.')
+
 
 def setup(bot):
     bot.add_cog(rTrakt(bot))
