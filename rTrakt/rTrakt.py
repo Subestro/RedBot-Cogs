6151a7a6c4db19c72ffac8e6e12e85cbf3a5219c
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-14 23:06:21 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,30 +1,24 @@
 import discord
-from discord.ext import commands, tasks
-from redbot.core import checks, commands, Config
-import trakt
-import asyncio
+from redbot.core import commands, checks, Config
 
 class rTrakt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.trakt_user = "subestro"  # Replace with your Trakt username
-        self.activity_task.start()
+        self.config = Config.get_conf(self, identifier=1234567890)  # Replace with a unique identifier
+        self.config.register_global(activity="")
 
-    def cog_unload(self):
-        self.activity_task.cancel()
+    @commands.Cog.listener()
+    async def on_ready(self):
+        activity = await self.config.activity()
+        if activity:
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
 
-    @tasks.loop(seconds=10)
-    async def activity_task(self):
-        await self.bot.wait_until_ready()
-        trakt_activity = await trakt.users(self.trakt_user).activity()
-        # Parse the Trakt activity and extract the relevant information
-        # You can customize how the activity is displayed in Discord
-        activity_text = f"Currently watching: {trakt_activity[0].type} {trakt_activity[0].show.title}"
-        await self.bot.change_presence(activity=discord.Game(name=activity_text))
+    @commands.command()
+    @checks.is_owner()
+    async def setactivity(self, ctx, *, activity: str):
+        await self.config.activity.set(activity)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        await ctx.send(f"Activity set to: {activity}")
 
 def setup(bot):
-    Trakt.configuration.defaults.client(
-        id='4129a600893f2b057301ef356e96277f0bf2898c205ff02e6dcfdeecef899b42',
-        secret='be487ee7080112b005cd8008157eceb022a14740e7f10bbc6c571803893dbda5'
-    )
     bot.add_cog(rTrakt(bot))
