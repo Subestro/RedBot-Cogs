e7017f1160869058cd01282b62a56cce91ce7a93
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-14 23:33:54 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -24,14 +24,14 @@ class rTrakt(commands.Cog):
         while not self.bot.is_closed():
             activity = await self.get_current_watching_activity()
             await self.update_bot_activity(activity)
-            await asyncio.sleep(5)  # Check every 5 second
+            await asyncio.sleep(5)  # Check every 5s
 
     async def get_current_watching_activity(self):
         try:
             trakt_config = await self.config.all()
-            client_id = trakt_config["client_id"]
-            client_secret = trakt_config["client_secret"]
-            access_token = trakt_config["access_token"]
+            client_id = await trakt_config.client_id()
+            client_secret = await trakt_config.client_secret()
+            access_token = await trakt_config.access_token()
 
             if client_id and client_secret and access_token:
                 trakt.core.AUTH_METHOD = trakt.OAuthAuthenticator(client_id, client_secret, access_token)
@@ -40,7 +40,7 @@ class rTrakt(commands.Cog):
                     return f"Watching {watched[0].movie.title}"
         except Exception as e:
             print(f"Error retrieving Trakt data: {e}")
-        return "Nothing"
+        return "Playing Nothing"
 
     async def update_bot_activity(self, activity):
         await self.config.activity.set(activity)
@@ -54,21 +54,20 @@ class rTrakt(commands.Cog):
     async def settraktcreds(self, ctx, client_id: str, client_secret: str):
         await self.config.client_id.set(client_id)
         await self.config.client_secret.set(client_secret)
-        await ctx.send(f"Please authorize the bot using the following link:\n\n{self.get_authorization_url()}")
+        await ctx.send(f"Please authorize the bot using the following link:\n\n{await self.get_authorization_url()}")
 
-    def get_authorization_url(self):
+    async def get_authorization_url(self):
         trakt_config = await self.config.all()
         client_id = await trakt_config.client_id()
         redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
         return f"https://trakt.tv/oauth/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
 
-
     @commands.command()
     @checks.is_owner()
     async def settrakttoken(self, ctx, authorization_code: str):
         trakt_config = await self.config.all()
-        client_id = trakt_config["client_id"]
-        client_secret = trakt_config["client_secret"]
+        client_id = await trakt_config.client_id()
+        client_secret = await trakt_config.client_secret()
         redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
 
         try:
