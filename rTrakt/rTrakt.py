3045930b2a597d9830e99501f3897449e428b768
Subestro <120358755+Subestro@users.noreply.github.com>
2023-06-15 21:34:49 -0700

    Update rTrakt.py

--- rTrakt/rTrakt.py
+++ rTrakt/rTrakt.py
@@ -1,94 +1,84 @@
 import discord
-from discord.ext import commands
-from redbot.core import commands, Config
-import trakt
-
+import asyncio
+from redbot.core import commands, checks, Config
+from trakt import Trakt
 
 class rTrakt(commands.Cog):
-    """Cog for interacting with the Trakt API."""
-
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)
-        default_global = {"client_id": None, "client_secret": None}
-        self.config.register_global(**default_global)
-        
-    async def get_trakt_client(self):
-        client_id = await self.config.client_id()
-        client_secret = await self.config.client_secret()
-        if not client_id or not client_secret:
-            raise Exception("Trakt API credentials are not set.")
-        trakt.core.oauth.OAuth.default_client(
-            id=client_id,
-            secret=client_secret
+        self.config = Config.get_conf(self, identifier=1234567890)  # Replace with a unique identifier
+        self.config.register_global(
+            client_id=None,
+            client_secret=None,
+            access_token=None,
+            activity=""
         )
-        return trakt
 
-    @commands.group(autohelp=True)
-    async def trakt(self, ctx):
-        """Interact with Trakt API."""
-        pass
+        self.task = bot.loop.create_task(self.update_activity())
 
-    @trakt.command()
-    async def settraktcreds(self, ctx: commands.Context, client_id: str, client_secret: str):
-        """Set Trakt API credentials."""
+    def cog_unload(self):
+        self.task.cancel()
+
+    async def update_activity(self):
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed():
+            activity = await self.get_current_watching_activity()
+            await self.update_bot_activity(activity)
+            await asyncio.sleep(2)  # Check every 2s
+
+    async def get_current_watching_activity(self):
         try:
-            await self.config.client_id.set(client_id)
-            await self.config.client_secret.set(client_secret)
-            await ctx.send("Trakt API credentials set successfully.")
-        except Exception as e:
-            await ctx.send(f"An error occurred while setting Trakt API credentials: {e}")
+            trakt_config = await self.config.all()
+            client_id = trakt_config["client_id"]
+            client_secret = trakt_config["client_secret"]
+            access_token = trakt_config["access_token"]
 
-    @trakt.command()
-    async def authorize(self, ctx: commands.Context):
-        """Authorize the bot with Trakt."""
-        try:
-            trakt_client = await self.get_trakt_client()
-            auth_url = trakt_client['oauth'].authorization_url('urn:ietf:wg:oauth:2.0:oob')
-            await ctx.send(f"Please authorize the bot using the following link:\n\n{auth_url}")
+            if client_id and client_secret and access_token:
+                Trakt.configuration.defaults.client(
+                    id=client_id,
+                    secret=client_secret
+                )
+                trakt = Trakt(access_token)
+                watched = await trakt['sync/watched'].movies()
+                if watched:
+                    return f"Watching {watched[0].title}"
         except Exception as e:
-            await ctx.send(f"An error occurred while authorizing the bot with Trakt: {e}")
+            print(f"Error retrieving Trakt data: {e}")
+        return "Playing Nothing"
 
-    @trakt.command()
-    async def settraktcode(self, ctx: commands.Context, code: str):
-        """Set the Trakt authorization code."""
-        try:
-            trakt_client = await self.get_trakt_client()
-            token = trakt_client['oauth'].token_exchange(code, 'urn:ietf:wg:oauth:2.0:oob')
-            await self.config.access_token.set(token["access_token"])
-            await self.config.refresh_token.set(token["refresh_token"])
-            await ctx.send("Trakt authorization code set successfully.")
-        except Exception as e:
-            await ctx.send(f"An error occurred while setting the Trakt authorization code: {e}")
+    async def update_bot_activity(self, activity):
+        await self.config.activity.set(activity)
+        if activity:
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+        else:
+            await self.bot.change_presence(activity=None)
 
-    @trakt.command()
-    async def scrobblestatus(self, ctx: commands.Context):
-        """Get the current scrobble status."""
-        try:
-            trakt_client = await self.get_trakt_client()
-            access_token = await self.config.access_token()
-            trakt_client['oauth'].access_token(access_token)
-            scrobble = trakt_client['users'].User.get_activities('me', limit=1)
-            
-            if scrobble:
-                scrobble = scrobble[0]
-                if scrobble.action == 'scrobble':
-                    if scrobble.type == 'movie':
-                        movie_title = scrobble.movie.title
-                        await ctx.send(f"The last scrobbled movie is: {movie_title}")
-                    elif scrobble.type == 'episode':
-                        show_title = scrobble.show.title
-                        episode_title = scrobble.episode.title
-                        await ctx.send(f"The last scrobbled episode is from the show: {show_title}, episode: {episode_title}")
-                    else:
-                        await ctx.send("The last scrobble is not a movie or an episode.")
-                else:
-                    await ctx.send("No scrobble activity found.")
-            else:
-                await ctx.send("No scrobble activity found.")
-        except Exception as e:
-            await ctx.send(f"An error occurred while retrieving the scrobble status: {e}")
+    @commands.command()
+    @checks.is_owner()
+    async def settraktcreds(self, ctx, client_id: str, client_secret: str, access_token: str):
+        await self.config.client_id.set(client_id)
+        await self.config.client_secret.set(client_secret)
+        await self.config.access_token.set(access_token)
+        await ctx.send("Trakt API credentials and access token have been set.")
+        await ctx.send(f"Please authorize the bot using the following link:\n\n{await self.get_authorization_url()}")
 
+    @commands.command()
+    @checks.is_owner()
+    async def setactivity(self, ctx, *, activity: str):
+        await self.update_bot_activity(activity)
+        await ctx.send(f"Activity set to: {activity}")
+
+    async def get_authorization_url(self):
+        trakt_config = await self.config.all()
+        client_id = trakt_config["client_id"]
+        redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+
+        Trakt.configuration.defaults.client(
+            id=client_id
+        )
+        auth = Trakt['oauth'].authorize_url(redirect_uri=redirect_uri)
+
+        return auth
 
 def setup(bot):
     bot.add_cog(rTrakt(bot))
